a5264f72d7f8e9d8ad023d013036c2510cad8819
OgabekDev <ogabekdev@gmail.com>
2021-12-10 23:17:37 +0500

    LifeCycle

--- app/src/main/java/com/example/pdp_ai/ui/data/MyLifeCycle.kt
+++ app/src/main/java/com/example/pdp_ai/ui/data/MyLifeCycle.kt
@@ -10,37 +10,37 @@ class MyLifeCycle(private val context: Context, private val myLifeCycle: Lifecyc
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreate() {
-        Log.i("Lifffffff", "ON_CREATE")
+        Log.i("Liffffff", "ON_CREATE")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStart() {
-        Log.i("Lifffffff", "ON_START")
+        Log.i("Liffffff", "ON_START")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
-        Log.i("Lifffffff", "ON_RESUME")
+        Log.i("Liffffff", "ON_RESUME")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
-        Log.i("Lifffffff", "ON_PAUSE")
+        Log.i("Liffffff", "ON_PAUSE")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onStop() {
-        Log.i("Lifffffff", "ON_STOP")
+        Log.i("Liffffff", "ON_STOP")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy() {
-        Log.i("Lifffffff", "ON_DESTROY")
+        Log.i("Liffffff", "ON_DESTROY")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
     fun onAny () {
-        Log.i("Lifffffff", "ON_ANY")
+        Log.i("Liffffff", "ON_ANY")
     }
 
 }
